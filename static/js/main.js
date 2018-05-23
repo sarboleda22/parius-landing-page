1567ece0d404f30bbce89445b8a3b11821e9e4e8
Sebastian Arboleda <sebasarboleda22@gmail.com>
2018-05-22 22:01:21 -0400

    Added responsive animated navbar

---
+++ static/js/main.js
@@ -0,0 +1,10 @@
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 1){  
+    $('.navbar').addClass("navbar-custom");
+    $('.navbar .navbar-brand img').attr('src','static/images/logo.png');
+  }
+  else {
+    $('.navbar').removeClass("navbar-custom");
+    $('.navbar .navbar-brand img').attr('src','static/images/logo-white.png');
+  }
+});
