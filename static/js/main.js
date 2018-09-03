73fdc6454624755cc3ad4a003f17d6db3b82d76a
Sebastian Arboleda <sebasarboleda22@gmail.com>
2018-09-03 14:22:15 -0400

    First iteration on main home section

--- static/js/main.js
+++ static/js/main.js
@@ -2,9 +2,11 @@ $(window).scroll(function() {
   if ($(this).scrollTop() > 1){  
     $('.navbar').addClass("navbar-custom");
     $('.navbar .navbar-brand img').attr('src','static/images/logo.png');
+    $('.navbar .navbar-brand img').attr('height','45');
   }
   else {
     $('.navbar').removeClass("navbar-custom");
     $('#desktop-logo').attr('src','static/images/logo-white.png');
+    $('#desktop-logo').attr('height','80');
   }
 });
