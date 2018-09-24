18bb8a8d2e00c3d307d3deb3237f0ae3e83525de
Sebastian Arboleda <sebasarboleda22@gmail.com>
2018-09-24 12:51:53 -0400

    Changed logo

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,12 @@
 $(window).scroll(function() {
   if ($(this).scrollTop() > 1){  
     $('.navbar').addClass("navbar-custom");
-    $('.navbar .navbar-brand img').attr('src','static/images/logo.png');
-    $('.navbar .navbar-brand img').attr('height','45');
+    $('.navbar .navbar-brand img').attr('src','static/images/horizontal_logo.png');
+    $('.navbar .navbar-brand img').attr('height','60');
   }
   else {
     $('.navbar').removeClass("navbar-custom");
-    $('#desktop-logo').attr('src','static/images/logo-white.png');
-    $('#desktop-logo').attr('height','80');
+    $('#desktop-logo').attr('src','static/images/new_edit_transparent.png');
+    $('#desktop-logo').attr('height','120');
   }
 });
