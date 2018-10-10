36de2b655a5cf23339fa96aaac97da963d4fa542
Sebastian Arboleda <sebasarboleda22@gmail.com>
2018-10-10 18:38:01 -0400

    Added Recursos, Contacto and pop up form

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,41 @@
-/*$(window).scroll(function() {
-  if ($(this).scrollTop() > 1){  
-    $('.navbar').addClass("navbar-custom");
-    $('.navbar .navbar-brand img').attr('src','static/images/horizontal_logo.png');
-    $('.navbar .navbar-brand img').attr('height','60');
-  }
-  else {
-    $('.navbar').removeClass("navbar-custom");
-    $('#desktop-logo').attr('src','static/images/new_edit_transparent.png');
-    $('#desktop-logo').attr('height','120');
-  }
-});*/
+// Get the modal
+var modal = document.getElementById('myModal');
+var successModal = document.getElementById('mySuccessModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+var btn2 = document.getElementById("myBtn2");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close")[1];
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+btn2.onclick = function() {
+    modal.style.display = "block";
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+span2.onclick = function() {
+  successModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal || event.target == successModal) {
+        modal.style.display = "none";
+        successModal.style.display = "none";
+    }
+}
+
+// Success Modal after inputting info in form
+$('#registrationForm').on('submit', function(e){
+  successModal.style.display = "block";
+});
+$('#registrationForm').on('submit', function() {
+  modal.style.display = "none";
+});
