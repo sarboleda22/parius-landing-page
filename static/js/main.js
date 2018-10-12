e1c75413ac1ae13df264d76c37a8a798ba11824d
Sebastian Arboleda <sebasarboleda22@gmail.com>
2018-10-12 12:10:19 -0400

    Final iteration

--- static/js/main.js
+++ static/js/main.js
@@ -5,14 +5,52 @@ var successModal = document.getElementById('mySuccessModal');
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 var btn2 = document.getElementById("myBtn2");
+var btn3 = document.getElementById("myBtn3");
+var btn4 = document.getElementById("myBtn4");
+var btn5 = document.getElementById("myBtn5");
+var btn6 = document.getElementById("myBtn6");
+var btn7 = document.getElementById("myBtn7");
+var btn8 = document.getElementById("myBtn8");
+var btn9 = document.getElementById("myBtn9");
+var btn10 = document.getElementById("myBtn10");
+var btn11 = document.getElementById("myBtn10");
+
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 var span2 = document.getElementsByClassName("close")[1];
+
 // When the user clicks on the button, open the modal 
 btn.onclick = function() {
     modal.style.display = "block";
 }
 btn2.onclick = function() {
+    modal.style.display = "block";
+}
+btn3.onclick = function() {
+    modal.style.display = "block";
+}
+btn4.onclick = function() {
+    modal.style.display = "block";
+}
+btn5.onclick = function() {
+    modal.style.display = "block";
+}
+btn6.onclick = function() {
+    modal.style.display = "block";
+}
+btn7.onclick = function() {
+    modal.style.display = "block";
+}
+btn8.onclick = function() {
+    modal.style.display = "block";
+}
+btn9.onclick = function() {
+    modal.style.display = "block";
+}
+btn10.onclick = function() {
+    modal.style.display = "block";
+}
+btn11.onclick = function() {
     modal.style.display = "block";
 }
 // When the user clicks on <span> (x), close the modal
